fef84e957112ab8cf594b5ae46907c730292808a
Mihail-Iliev13 <mihaililiev13@gmail.com>
2018-10-16 11:20:57 +0300

    initial commit

---
+++ src/main/java/com/telerikacademy/drivingcardserver/DrivingCardServerApplication.java
@@ -0,0 +1,12 @@
+package com.telerikacademy.drivingcardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingCardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingCardServerApplication.class, args);
+    }
+}
