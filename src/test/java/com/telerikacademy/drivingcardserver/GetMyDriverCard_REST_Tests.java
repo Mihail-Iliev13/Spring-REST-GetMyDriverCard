ee4eb89ed15c3e623beffe04900c223a4c0b95f4
todorslavov <todordelyanovslavov72@gmail.com>
2018-11-11 14:39:38 +0200

    UnitTests for CardApplication service

--- src/test/java/com/telerikacademy/drivingcardserver/GetMyDriverCard_REST_Tests.java
+++ src/test/java/com/telerikacademy/drivingcardserver/GetMyDriverCard_REST_Tests.java
@@ -19,25 +19,39 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import static com.telerikacademy.drivingcardserver.models.enums.CardApplicationStatus.APPROVED;
+import static com.telerikacademy.drivingcardserver.models.enums.CardApplicationStatus.NEW;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetMyDriverCard_REST_Tests {
 
 User user=new User();
-Date date1= new Date(11,11,2011);
+String date1="2011-11-11";
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    Date filterDate = dateFormat.parse(date1);
+    String date1s = dateFormat.format(filterDate);
+    Date date2= new Date(12,12,2012);
 PersonalDetails persDetails= new PersonalDetails("9602180077","Todor","Slavov");
+   CardApplication ca4 = new CardApplication(user,4,filterDate, CardApplicationStatus.NEW,persDetails);
     List<CardApplication> defaultTestInput = Arrays.asList(
-            new CardApplication(user,1,date1, CardApplicationStatus.NEW,persDetails),
-            new CardApplication(user,2,date1, CardApplicationStatus.NEW,persDetails),
-            new CardApplication(user,3,date1, CardApplicationStatus.NEW,persDetails)
+            new CardApplication(user,1,filterDate, CardApplicationStatus.NEW,persDetails),
+            new CardApplication(user,2,date2, CardApplicationStatus.NEW,persDetails),
+            new CardApplication(user,3,date2, CardApplicationStatus.NEW,persDetails)
+
 
     );
+
+    public GetMyDriverCard_REST_Tests() throws ParseException {
+    }
 
     @Test
     public void getAllCardApplicationsShouldReturnAllCardApplications() {
@@ -56,7 +70,130 @@ PersonalDetails persDetails= new PersonalDetails("9602180077","Todor","Slavov");
         Assert.assertEquals(3, result.size());
 
     }
+    @Test
+    public void getCardApplicationByIDShouldReturnApplicationWithCertainID() {
+        // Arrange
+        CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+        Mockito.when(mockRepository.getApplicationByID(2))
+                .thenReturn(defaultTestInput.get(1));
+        CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
 
+
+
+        // Act
+        CardApplication result =mockService.getApplicationByID(2);
+
+        // Assert
+        Assert.assertEquals(2, result.getId());
+
+    }
+    @Test
+    public void CreateCardApplicationShouldReturnNewApplication() {
+        // Arrange
+        CardApplication ca4 = new CardApplication(user,4,filterDate, CardApplicationStatus.NEW,persDetails);
+
+        CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+        Mockito.when(mockRepository.createCardApplication(ca4))
+                .thenReturn(ca4);
+
+        CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
+
+
+        // Act
+        CardApplication result = mockService.createCardApplication(ca4);
+
+
+        // Assert
+        Assert.assertEquals(4, result.getId());
+
+    }
+@Test
+public void UpdateCardAppStatusAndCheckIfItHasChanged() {
+    // Arrange
+    CardApplication ca5 = new CardApplication(user,4,filterDate, CardApplicationStatus.APPROVED,persDetails);
+    CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+    Mockito.when(mockRepository.updateCardApplication(4,APPROVED))
+            .thenReturn(ca5);
+    CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
+
+
+
+    // Act
+    CardApplication result =mockService.updateCardApplicationStatus(4, APPROVED);
+
+    // Assert
+    Assert.assertEquals(APPROVED, result.getStatus());
+
+}
+    @Test
+    public void getCardApplicationsFilteredByName() {
+        // Arrange
+        CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+        Mockito.when(mockRepository.getAllApplications())
+                .thenReturn(defaultTestInput);
+        CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
+
+
+
+        // Act
+        List<CardApplication> result =mockService.getFilteredByName("Todor");
+
+        // Assert
+        Assert.assertEquals("Todor", result.get(0).getDetails().getFirstNameLatin());
+
+    }
+    @Test
+    public void getCardApplicationsFilteredByID() {
+        // Arrange
+        CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+        Mockito.when(mockRepository.getAllApplications())
+                .thenReturn(defaultTestInput);
+        CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
+
+
+
+        // Act
+        List<CardApplication> result =mockService.getFilteredByID("9602180077");
+
+        // Assert
+        Assert.assertEquals("9602180077", result.get(0).getDetails().getDriverID());
+
+    }
+    @Test
+    public void getCardApplicationsFilteredByStatus() {
+        // Arrange
+        CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+        Mockito.when(mockRepository.getAllApplications())
+                .thenReturn(defaultTestInput);
+        CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
+
+
+
+        // Act
+        List<CardApplication> result =mockService.getFilteredByStatus("NEW");
+
+        // Assert
+        Assert.assertEquals(NEW, result.get(0).getStatus());
+
+    }
+    @Test
+    public void getCardApplicationsFilteredByDate() throws ParseException {
+        // Arrange
+        CardApplicationRepositoryImpl mockRepository= Mockito.mock(CardApplicationRepositoryImpl.class);
+        Mockito.when(mockRepository.getAllApplications())
+                .thenReturn(defaultTestInput);
+        CardApplicationService mockService=new CardApplicationServiceImpl(mockRepository);
+
+
+
+        // Act
+        List<CardApplication> result =mockService.getFillteredByDate("2011-11-11");
+
+        // Assert
+        CardApplication cardApp = result.get(0);
+        Assert.assertEquals(filterDate, cardApp.getDateOfSubmission());
+
+    }
 
 
 
